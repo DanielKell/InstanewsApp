7a37057cb2cc37c77aeaeca82f23c2373706bf3d
DanielKell <danielkell.ubc@gmail.com>
2017-07-24 18:18:27 -0700

    Reorganization of file structure, cleaning up code, etc
    -Created mixins and variables with Sass, to use in the CSS file
    -Removed some old dud files
    -Cleaned up code in all of the files; spacing, indentation, comments,
    etc

--- README.md
+++ README.md
@@ -3,4 +3,3 @@ The goal of this project was to create a website that looks similar to an app in
 The main learning elements of this project included using JQuery to dynamically change the structure of the website when options in a <select> button are picked. Using Ajax the data is pulled from the API and populated into fields. These fields change dynamically based on width of the browser using Flexbox.
 
 The structure of this project was also different to previous in that I used Sass to write my CSS and Gulp to automate many of the tasks such as minifying and outputting into a build folder, grabbing vendor prefixes, and linting.
-
--- js/functionality.js
+++ js/functionality.js
@@ -1,65 +1,64 @@
 
 $(document).ready(function(){
 
-//Starting the JQuery and JS functions when the category is clicked
-$('#category').on('change', function(){
-  $('.loader').show();
-  $('.footer').css('position', 'relative');
-  $('.main-body').addClass('main-body-populated');
+  //Starting the JQuery and JS functions when the category is clicked
+  $('#category').on('change', function(){
+    $('.loader').show();
+    $('.footer').css('position', 'relative');
+    $('.main-body').addClass('main-body-populated');
 
-//Defining and setting up the url to pull data based on section chosen
-var newstype = $('#category').val();
-var url = 'https://api.nytimes.com/svc/topstories/v2/'; 
-url += newstype + '.json?' + $.param({
-  'api-key': '49ff125fe406457b9390fdf0225c1a6f'
-});
+    //Defining and setting up the url to pull data based on section chosen
+    var newstype = $('#category').val();
+    var url = 'https://api.nytimes.com/svc/topstories/v2/'; 
+    url += newstype + '.json?' + $.param({
+      'api-key': '49ff125fe406457b9390fdf0225c1a6f'
+    });
 
-//Clearing the contents before the data is requested so it's not loaded multiple times.
- $('.news-list li').remove();
+    //Clearing the contents before the data is requested so it's not loaded multiple times.
+    $('.news-list li').remove();
 
-//Begin Ajax call
-$.ajax({
-  url: url,
-  method: 'GET',
-})
+    //Begin Ajax call
+    $.ajax({
+      url: url,
+      method: 'GET',
+    })
 
- .done(function(data) {
+    .done(function(data) {
 
-/*Creating a new array that contains only the data for the first 12 items WITH images. 
-  Filter creates a new array and returns only the values that satisfy the condition. We then slice to 
-  contain it to the first 12 items, and instead of using data.results in the $.each function, use the new variable.*/
-  var imagesTrue = data.results.filter(function(imagesFilter) {
-  return imagesFilter.multimedia.length > 0;
-  }).slice(0,12); 
-    
-  //console.log(data);
+    /*Creating a new array that contains only the data for the first 12 items WITH images. 
+    Filter creates a new array and returns only the values that satisfy the condition. We then slice to 
+    contain it to the first 12 items, and instead of using data.results in the $.each function, use the new variable.*/
+    var imagesTrue = data.results.filter(function(imagesFilter) {
+    return imagesFilter.multimedia.length > 0;
+    }).slice(0,12); 
 
-  //pulling data for each of the filtered elements and creating new data structures to hold the data in the .news-list class
-  $.each(imagesTrue, function(key, article) {
-  $('.news-list').append('<li>' 
-  + '<a href="' 
-  + article.url
-  + '">'
-  + '<div class="inside-wrapper">'
-  + '<div class="article" style="background-image:url(' + article.multimedia[4].url + ')">'
-  + '<div class="story-content">'
-  + '<p>'
-  + article.abstract
-  + '</p>'
-  + '</div>'
-  + '</div>'
-  + '</div>'
-  + '</a>'
-  + '</li>');
-  })
+    //pulling data for each of the filtered elements and creating new data structures to hold the data in the .news-list class
+    $.each(imagesTrue, function(key, article) {
+    $('.news-list').append('<li>' 
+    + '<a href="' 
+    + article.url
+    + '">'
+    + '<div class="inside-wrapper">'
+    + '<div class="article" style="background-image:url(' + article.multimedia[4].url + ')">'
+    + '<div class="story-content">'
+    + '<p>'
+    + article.abstract
+    + '</p>'
+    + '</div>'
+    + '</div>'
+    + '</div>'
+    + '</a>'
+    + '</li>');
+    })
 
-}) //Ending .done function
+    }) //Ending .done function
 
-  .fail(function(err) {
-  throw err;
-})
-  .always(function() { //Hide the loader button here; show it on the onclick.
-  $('.loader').hide();
+    .fail(function(err) {
+    throw err;
+    })
+
+    .always(function() { //Hide the loader button here; show it on the onclick.
+    $('.loader').hide();
     });
 
   }); //Ending the on change function
